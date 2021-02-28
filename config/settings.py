9f21f577c00556c627d85c9e65f36b2f8f6cea14
m2033099 <m1610600@gmail.com>
2021-02-28 16:42:53 +0900

    first commit

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#f$*^qoq%2cqjosbc58t7(x#r%+caak(ef1@)ii(a$6riap$2c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
